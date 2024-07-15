4145d7ad968a8bba098ae161b6ddd9ab44aff022
Anders Skibsted <abo@internettet.email>
2024-07-15 19:08:40 +0000

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -10,9 +11,15 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+let urlShortened = {};
+let numberOfUrls = 0;
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -22,3 +29,18 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  numberOfUrls++;
+  urlShortened[numberOfUrls] = originalUrl;
+  res.json({ 'original_url': originalUrl, 'short_url': numberOfUrls });
+
+})
+
+app.get('/api/shorturl/:number', (req, res) => {
+  const redirectUrl = urlShortened[req.params.number];
+  res.redirect(redirectUrl);
+
+})
+
